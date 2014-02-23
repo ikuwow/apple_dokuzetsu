f3588f4134562cf647ec15c08cc86ec30bfa6834
Ikuo Degawa <degawa@ikuwow.com>
2014-02-23 19:06:46 +0900

    modify tweet script using pdo

--- tweet.php
+++ tweet.php
@@ -7,6 +7,16 @@ require_once("functions.php");
 require_once("config.php");
 
 
+// MySQLに接続
+try {
+    $dsn = 'mysql:host='.DB_HOST.';dbname='.DB_NAME;
+    $dbh = new PDO($dsn,DB_USER,DB_PASSWORD);
+    $dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    var_dump($e->getMessage());
+    exit;
+}
+
 // OAuthオブジェクト生成
 $to = new TwitterOAuth(
 	CONSUMER_KEY,
@@ -15,70 +25,59 @@ $to = new TwitterOAuth(
 	ACCESS_TOKEN_SECRET
 );
 
-// MySQLに接続
-$MySQLlink = mysql_connect(DB_HOST,DB_USER,DB_PASSWORD);
-if (!$MySQLlink) {
-	die('MySQLへの接続に失敗しました。');
-}
-
-// データベースの選択
-$db_selected = mysql_select_db(DB_NAME,$MySQLlink);
-if (!$db_selected) {
-	die('データベースの選択に失敗しました。');
-}
-
 // ツイートを重み付きランダムに取得
-$tweetnum = mysql_query('SELECT ID,num_of_times FROM tweets ORDER BY ID;');
+$stmt = $dbh->query('SELECT * FROM tweets ORDER BY ID');
 
-$itr = 0;
-while ($row = mysql_fetch_assoc($tweetnum,MYSQL_NUM) ) {
-  $num_of_times[$itr] = $row[1];
-  $itr = $itr + 1;
+$tweets = $stmt->fetchAll();
+$nums = array();
+foreach ($tweets as $tweet) {
+    $nums[] = $tweet['num_of_times'];
+}
+$max_weight = max($nums);
+
+$weights = array();
+foreach ($nums as $key => $num) {
+  $weights[$key] = $max_weight-$num+1;
 }
 
-foreach ($num_of_times as $key => $num) {
-  $weights[$key] = max($num_of_times)-$num+1;
-}
-$tmp = weighted_random($weights);
-$tweetid = $tmp+1; //インデックスを1始まりに
+$tw_idx = weighted_random($weights);
+$tweet_id = $tw_idx+1; //インデックスを1始まりに
 
-$select_tweet = "SELECT tweet FROM tweets WHERE ID = $tweetid ;";
-$tweet_ = mysql_fetch_assoc(mysql_query($select_tweet));
-$tweet = $tweet_['tweet'];
+// var_dump($tweets[$tw_idx]);
 
 // TwitterへPOSTする。パラメーターは配列に格納する
 $req = $to->OAuthRequest(
   "https://api.twitter.com/1.1/statuses/update.json",
   "POST",
-  array("status"=>"$tweet")
+  array("status"=>$tweets[$tw_idx]['tweet'])
 );
 
-$result = json_decode($req);
+// $result = json_decode($req);
 
 // echo "<pre>";
 // var_dump($result);
 
 // ツイート回数を更新
-$numoftimes = mysql_fetch_assoc(
-  mysql_query(
-  'SELECT num_of_times AS num FROM tweets WHERE ID = '."$tweetid".';'
-  )
-);
-$numoftimes = (int) $numoftimes['num'];
+// $numoftimes = (int) $numoftimes['num'];
 
-$increment = 'UPDATE tweets SET num_of_times = '
-	      ."$numoftimes+1".' WHERE ID = '."$tweetid".' ;';
-$count_twinum = mysql_query($increment);
-if ($count_twinum) {
-  $tmp = $numoftimes +1;
-  print "Tweet $tweetid was incremented to $tmp.\n";
+// $increment = 'UPDATE tweets SET num_of_times = '
+// ."$numoftimes+1".' WHERE ID = '."$tweetid".' ;';
+// $count_twinum = mysql_query($increment);
+
+$sql = 'UPDATE tweets set num_of_times = :num where ID = :id';
+$stmt = $dbh->prepare($sql);
+$stat = $stmt->execute(array(':num'=>$tweets[$tw_idx]['num_of_times']+1,':id'=>$tweet_id));
+
+if ($stat) {
+  $tmp = $tweets[$tw_idx]['num_of_times'] +1;
+  print "Tweet $tweet_id was incremented to $tmp.\n";
 } else {
   print "Cannot incremented.";
 };
 
+/*
 $close_flag = mysql_close($MySQLlink);
 if (!$close_flag) { 
   die('Cannot disconnect MySQL.');
 }
-
-?>
+ */
