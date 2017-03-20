a959759e504230cbc00017f1964ecf83920eed70
Ikuo Degawa <degawa@ikuwow.com>
2017-03-20 09:12:54 +0900

    fix scheema and add base data

--- data/schema.sql
+++ data/schema.sql
@@ -1,10 +1,9 @@
 create table if not exists tweets (
-    id rowid,
+    id integer not null primary key,
     tweet varchar(255) not null,
     created integer not null default now
 );
 create table if not exists tweet_logs (
-    id rowid,
     tweet_id int not null,
-    created integer not null default now
+    tweeted integer not null default now
 );
